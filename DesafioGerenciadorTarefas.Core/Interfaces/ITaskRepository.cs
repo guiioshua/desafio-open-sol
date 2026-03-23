ca719eb8ea2375b24250c926bdd298c2cc0fa3c7
guiioshua <guilhermeioshua@gmail.com>
2026-03-23 20:05:09 -0300

    feat(core): definição da interface do repositório no domínio

---
+++ DesafioGerenciadorTarefas.Core/Interfaces/ITaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using DesafioGerenciadorTarefas.Core.Entities;
+using DesafioGerenciadorTarefas.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioGerenciadorTarefas.Core.Interfaces
+{
+    public interface ITaskRepository
+    {
+        // Task se refere Task do .NET, não TaskItem
+        Task AddAsync(TaskItem task);
+        Task<TaskItem?> GetByIdAsync(Guid id);
+
+        // Paginação por offset
+        Task<(IEnumerable<TaskItem> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize, StatusTask? statusFilter = null);
+
+        Task UpdateAsync(TaskItem task);
+        Task DeleteAsync(TaskItem task);
+    }
+}
