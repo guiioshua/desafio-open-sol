87811f0da6ae5178ae9e36cbe7c0370b7e272fec
guiioshua <guilhermeioshua@gmail.com>
2026-03-23 01:45:18 -0300

    feat(core): adição de modelos de exceções específicos para o domínio do app para desacoplamento de tratamento de exceções em um futuro handler global

---
+++ DesafioGerenciadorTarefas.Core/Enums/ErrorCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioGerenciadorTarefas.Core.Enums
+{
+    public enum ErrorCode
+    {
+        TitleRequired,
+        CompletedTaskCannotBeChanged,
+        InvalidStateTransitionToInProgress,
+        InvalidStateTransitionToDone,
+        TaskNotFound
+    }
+}
---
+++ DesafioGerenciadorTarefas.Core/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+﻿using DesafioGerenciadorTarefas.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioGerenciadorTarefas.Core.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public ErrorCode Code { get; }
+        public DomainException(ErrorCode code) : base(code.ToString()) 
+        {
+            Code = code;
+        }
+    }
+}
