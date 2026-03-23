2a9de685169a7ac73b7ecc86cfbdc9c6b9cc9ac9
guiioshua <guilhermeioshua@gmail.com>
2026-03-22 23:09:12 -0300

    Primeiro Commit. Criação das camadas da aplicação, boilerplates via templates de webapi e configuração das soluções

---
+++ DesafioGerenciadorTarefas.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace DesafioGerenciadorTarefas.Core;
+
+public class Class1
+{
+
+}
---
+++ DesafioGerenciadorTarefas.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DesafioGerenciadorTarefas.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
